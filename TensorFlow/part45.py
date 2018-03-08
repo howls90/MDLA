bd1537226b53ac5bad4c111975923efb3443647f
Martí Floriach <mfloriach90@gmail.com>
2018-03-08 11:51:57 +0900

    changes

--- TensorFlow/part45.py
+++ TensorFlow/part45.py
@@ -4,6 +4,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('/tmp/data/', one_hot=True)
 
 n_nodes_hl1 = 500
+asdbadbsjdsba
 n_nodes_hl2 = 500
 n_nodes_hl3 = 500
 
@@ -39,8 +40,8 @@ def neural_network_model(data):
 
 	output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
 
-	return output
-	
+	pass
+
 def train_neural_network(x):
 	prediction = neural_network_model(x)
 	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
@@ -57,7 +58,7 @@ def train_neural_network(x):
 				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
 				_, c = sess.run([optimitzer, cost], feed_dict={x:epoch_x,y:epoch_y})
 				epoch_loss += c
-			
+
 			print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
 
 		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
@@ -66,6 +67,6 @@ def train_neural_network(x):
 
 
 train_neural_network(x)
-
+train neural_network(x)
 
 
